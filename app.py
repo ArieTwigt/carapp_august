eb74fc18a39ab8f85c06040cca5c8bab64ae4f0c
arietwigt <twigt.arie@gmail.com>
2022-08-10 12:00:28 +0200

    added input and display of tables

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # imported the Flask class
-from flask import Flask, render_template
-from custom_modules.api_functions import get_car_by_plate
+from crypt import methods
+from flask import Flask, render_template, request
+from custom_modules.api_functions import get_car_by_plate, random_cars_brand
 
 # initiated application
 app = Flask(__name__)
@@ -18,6 +19,16 @@ def show_car():
     car = get_car_by_plate("TB725F")
     return render_template("show_car.html", car=car)
 
+# route for returning a list of cars
+@app.route('/show_random_cars', methods=['GET', 'POST'])
+def show_random_cars():
+    
+    if request.method == 'POST':
+        selected_brand = request.form.get('brand')
+        cars = random_cars_brand(selected_brand)
+        return render_template("random_cars_brand.html", cars=cars)
+    
+    return render_template("random_cars_brand.html")
 
 # execute/run the application
 if __name__ == '__main__':
--- custom_modules/api_functions.py
+++ custom_modules/api_functions.py
@@ -7,3 +7,10 @@ def get_car_by_plate(plate):
     data = resp.json()
     car = data[0]
     return car
+
+
+def random_cars_brand(brand):
+    endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={brand}"
+    resp = requests.get(endpoint)
+    cars_list = resp.json()
+    return cars_list
