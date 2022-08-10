23d13793d29ef56b34673fee9d1a7eb450348a55
arietwigt <twigt.arie@gmail.com>
2022-08-10 11:30:58 +0200

    added the first templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # imported the Flask class
-from flask import Flask
+from flask import Flask, render_template
 from custom_modules.api_functions import get_car_by_plate
 
 # initiated application
@@ -8,13 +8,15 @@ app = Flask(__name__)
 # defined a route
 @app.route('/')
 def hello():
-    return "Hello Lisbon"
+    message = "Hello World"
+    return render_template("index.html",
+                           message=message)
 
 # route for getting the car by plate
 @app.route('/show_car')
 def show_car():
     car = get_car_by_plate("TB725F")
-    return car
+    return render_template("show_car.html", car=car)
 
 
 # execute/run the application
